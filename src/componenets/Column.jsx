3f5676583a2e07a81c81ff07123b996ef8058178
“Adi91108” <emailaddr>
2025-06-16 01:27:22 +0530

    new fix

--- src/componenets/Column.jsx
+++ src/componenets/Column.jsx
@@ -3,18 +3,18 @@ import TodoCard from "./TodoCard";
 import { BiPlus } from "react-icons/bi";
 
 const Column = ({
-    column,
-    tasks,
-    addsubsection,
-    setTodoModal,
-    editdetails,
-    handleAdd,
-    deletesubsection,
-    Filter,
-  }) => {
-    const { setNodeRef } = useDroppable({
-        id: column.id,
-      });
+  column,
+  tasks,
+  addsubsection,
+  setTodoModal,
+  editdetails,
+  handleAdd,
+  deletesubsection,
+  Filter,
+}) => {
+  const { setNodeRef } = useDroppable({
+    id: column.id,
+  });
 
   return (
     <div className="flex  w-full md:w-[33%] mb-6 flex-col rounded-lg bg-[#F5F5F5] gap-3 p-4">
@@ -25,7 +25,9 @@ const Column = ({
       >
         <div className="flex justify-center items-center gap-3">
           <div
-            className={`w-2 h-2 rounded-full ${`bg-${column.color}`} `}
+            className={`w-2 h-2 rounded-full ${
+              column?.id === "IN_PROGRESS" ? "bg-yellow-300" : ""
+            } ${column?.id !== "IN_PROGRESS" ? `bg-[${column?.color}]` : ""}`}
           ></div>
           <p className="font-semibold">{column.title}</p>
           <div className=" flex justify-center bg- items-center text-xs p-1 w-[20px] h-[20px] rounded-3xl bg-[#c0bfc3] ">
@@ -53,7 +55,11 @@ const Column = ({
           ""
         )}
       </div>
-      <div className={`w-[96%] h-[3px] ${`bg-${column?.color}`}`}></div>
+      <div
+        className={`w-[96%] h-[3px]  ${
+          column?.id === "IN_PROGRESS" ? "bg-yellow-300" : ""
+        } ${column?.id !== "IN_PROGRESS" ? `bg-[${column?.color}]` : ""}`}
+      ></div>
 
       <div ref={setNodeRef} className="flex flex-1 flex-col gap-4">
         {Filter !== null
@@ -86,6 +92,6 @@ const Column = ({
       </div>
     </div>
   );
-}
+};
 
-export default Column
+export default Column;
